6b2800eb87b1f032fe2b91dcdabf134bbe9bfd60
Endy Muhardin <endy.muhardin@gmail.com>
2021-08-22 15:51:04 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/vault/BelajarVaultApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarVaultApplication.class, args);
+	}
+
+}
