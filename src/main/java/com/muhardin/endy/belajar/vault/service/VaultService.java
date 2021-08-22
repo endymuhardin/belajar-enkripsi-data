bfa87a04d8b8d56a2f9b4a32aa6e8a75cf04964a
Endy Muhardin <endy.muhardin@gmail.com>
2021-08-22 18:50:19 +0700

    implement encrypt dan decrypt file

--- src/main/java/com/muhardin/endy/belajar/vault/service/VaultService.java
+++ src/main/java/com/muhardin/endy/belajar/vault/service/VaultService.java
@@ -1,11 +1,19 @@
 package com.muhardin.endy.belajar.vault.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.vault.core.VaultOperations;
 import org.springframework.vault.core.VaultTransitOperations;
 import org.springframework.vault.support.VaultTransitKeyCreationRequest;
 
-@Service
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Base64;
+import java.util.UUID;
+
+@Service @Slf4j
 public class VaultService {
     private static final String KEY_TYPE = "aes128-gcm96";
     private static final String KEY_ENCRYPT_STRING = "KEY_ENCRYPT_STRING";
@@ -24,5 +32,34 @@ public class VaultService {
 
     public String decrypt(String cipherText) {
         return vaultTransit.decrypt(KEY_ENCRYPT_STRING, cipherText);
+    }
+
+    public File encrypt(File plainFile){
+        try {
+            String base64Encoded = Base64.getEncoder().encodeToString(FileUtils.readFileToByteArray(plainFile));
+            String base64Encrypted = encrypt(base64Encoded);
+            File result = File.createTempFile(plainFile.getName()+"%", "-enc.txt");
+            FileUtils.writeStringToFile(
+                    result,
+                    base64Encrypted, Charset.defaultCharset());
+            return result;
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    public File decrypt(File cipherFile) {
+        try {
+            String cipherFileContent = FileUtils.readFileToString(cipherFile, Charset.defaultCharset());
+            String base64Encoded = decrypt(cipherFileContent);
+            File result = File.createTempFile(UUID.randomUUID().toString(), ".png");
+            byte[] decryptedFileContent = Base64.getDecoder().decode(base64Encoded);
+            FileUtils.writeByteArrayToFile(result, decryptedFileContent);
+            return result;
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
     }
 }
--- src/test/java/com/muhardin/endy/belajar/vault/service/VaultServiceTests.java
+++ src/test/java/com/muhardin/endy/belajar/vault/service/VaultServiceTests.java
@@ -3,10 +3,21 @@ package com.muhardin.endy.belajar.vault.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootTest
 public class VaultServiceTests {
+    @Value("classpath:sample-file/ktp-plain.png")
+    private Resource fileKtpPlain;
+
+    @Value("classpath:sample-file/ktp-encrypted.txt")
+    private Resource fileKtpEncrypted;
+
     @Autowired private VaultService vaultService;
 
     @Test
@@ -23,5 +34,17 @@ public class VaultServiceTests {
         String decryptedNik = vaultService.decrypt(encryptedNik);
         Assertions.assertNotNull(decryptedNik);
         System.out.println("Decrypted : " + decryptedNik);
+    }
+
+    @Test
+    public void testEncryptFile() throws Exception {
+        File encryptedKtp = vaultService.encrypt(fileKtpPlain.getFile());
+        System.out.println("Encrypted file : " + encryptedKtp.getAbsolutePath());
+    }
+
+    @Test
+    public void testDecryptFile() throws IOException {
+        File decryptedKtp = vaultService.decrypt(fileKtpEncrypted.getFile());
+        System.out.println("Decrypted file : "+ decryptedKtp.getAbsolutePath());
     }
 }
