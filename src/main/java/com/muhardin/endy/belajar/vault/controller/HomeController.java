e1282c460666087d9b04803231c30bfeeeca0979
Endy Muhardin <endy.muhardin@gmail.com>
2021-09-15 16:24:56 +0700

    fix link untuk membuka database dan folder

--- src/main/java/com/muhardin/endy/belajar/vault/controller/HomeController.java
+++ src/main/java/com/muhardin/endy/belajar/vault/controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.muhardin.endy.belajar.vault.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Value("${file.upload.folder}")
+    private String fileUploadFolder;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("fileUploadFolder", fileUploadFolder);
         return "home";
     }
 }
